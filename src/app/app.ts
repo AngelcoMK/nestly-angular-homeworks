f4ed7055dea2bbbd1aff57b50e9017074b7d2565
Angelco <ampevangelco3@gmail.com>
2026-09-12 00:56:53 +0200

    Add Homework 1 - Nestly

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { StayCardComponent } from './components/stay-card/stay-card';
+import { stays } from './models/stays';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [StayCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('nestly');
+  stays = stays;
+
+  favoriteIds = signal<number[]>([]);
+
+  showSuperhostOnly = signal(false);
+
+  get filteredStays() {
+    if (!this.showSuperhostOnly()) {
+      return this.stays;
+    }
+
+    return this.stays.filter(stay => stay.superhost);
+  }
+
+  onFavorite(stayId: number): void {
+    const currentFavorites = this.favoriteIds();
+
+    if (currentFavorites.includes(stayId)) {
+      this.favoriteIds.set(
+        currentFavorites.filter(id => id !== stayId)
+      );
+    } else {
+      this.favoriteIds.set([
+        ...currentFavorites,
+        stayId
+      ]);
+    }
+  }
+
+  toggleSuperhostFilter(): void {
+    this.showSuperhostOnly.update(value => !value);
+  }
 }
---
+++ src/app/components/stay-card/stay-card.ts
@@ -0,0 +1,29 @@
+import { Component, input, output } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Stay } from '../../models/stay.model';
+import { TruncatePipe } from '../../pipes/truncate-pipe';
+import { BadgeComponent } from '../badge/badge';
+import { HighlightCardDirective } from '../../directives/highlight-card';
+
+@Component({
+  selector: 'app-stay-card',
+  imports: [
+    TruncatePipe,
+    BadgeComponent,
+    HighlightCardDirective,
+    CurrencyPipe
+  ],
+  templateUrl: './stay-card.html',
+  styleUrl: './stay-card.css'
+})
+export class StayCardComponent {
+  stay = input.required<Stay>();
+
+  isFavorite = input(false);
+
+  favorite = output<number>();
+
+  onFavorite(): void {
+    this.favorite.emit(this.stay().id);
+  }
+}
